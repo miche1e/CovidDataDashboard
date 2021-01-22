049b23301cfb3995e3685124607bca0c2860f7f3
Caterina Natali <caterinanatali12@gmail.com>
2021-01-22 11:09:06 +0100

    fixed error and update routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatButtonModule } from '@angular/material/button';
@@ -15,15 +14,16 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './main/dashboard/dashboard.component';
-import {NgxChartsModule} from '@swimlane/ngx-charts';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    feature-toolBar
     NavBarComponent,
+    DashboardComponent,
   ],
+
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
@@ -33,16 +33,13 @@ import {NgxChartsModule} from '@swimlane/ngx-charts';
     MatSidenavModule,
     MatIconModule,
     MatListModule,
-    AppRoutingModule
-    DashboardComponent
-  ],
-  imports: [
+    AppRoutingModule,
     BrowserModule,
-    Dashboard_Component
-    NgxChartsModule
+    NgxChartsModule,
     FormsModule,
     BrowserAnimationsModule
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
