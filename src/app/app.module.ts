b26c82c0a21e9462d7595719d7fdc94c3a5747c8
StefanoSGTECH <38039913+StefanoSGTECH@users.noreply.github.com>
2021-01-22 11:54:04 +0100

    Add Flex

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
@@ -37,7 +38,8 @@ import { DashboardComponent } from './main/dashboard/dashboard.component';
     BrowserModule,
     NgxChartsModule,
     FormsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FlexLayoutModule
   ],
 
   providers: [],
